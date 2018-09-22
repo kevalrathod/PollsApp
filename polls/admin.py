2d9ddc60e0baa51d7613b235d34e5c8f0dace3be
kevalrathod <kevalrathod1111@gmail.com>
2018-09-22 23:24:56 +0530

    polls app complete

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,26 @@ from .models import Question,Choice
 
 
 # Register your models here.
-admin.site.register(Question)
+
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['que_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('que_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['que_text']
+
+
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
