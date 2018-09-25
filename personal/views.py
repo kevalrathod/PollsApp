ead48c521626e10b22a090cff12bf13f2d87a62c
kevalrathod <kevalrathod1111@gmail.com>
2018-09-25 17:31:26 +0530

    Adding blog and display blogs

--- personal/views.py
+++ personal/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request,'personal/home.html')
+
+def contact(request):
+	return render(request,'personal/basic.html',{'content':['If you would like to email me ,please send us', 'kevalrathod1111@gmail.com']})
