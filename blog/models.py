9980db1114f8a80488a934642c18150a51687a07
kevalrathod <kevalrathod1111@gmail.com>
2018-10-13 11:39:15 +0530

    adding new models

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,8 @@ class Post(models.Model):
     title=models.CharField(max_length=140) 
     body=models.TextField(max_length=500)
     date=models.DateTimeField("Published")
-    publisher = models.CharField(max_length=200)
+    slug = models.SlugField(null=True)
+    publisher=models.CharField(max_length=200,default='some-string')
 
 
     def __str__(self):
